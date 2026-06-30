62990c86f1a01d4a0178a4932743413a90817a2f
BardMoon <came.from.the.north7239@gmail.com>
2026-07-01 02:48:56 +0900

    add Dropdown

---
+++ src/lib/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Button } from "./button/Button.svelte";
+export { default as DropdownButton } from "./button/DropdownButton.svelte";
+
+export { default as Dropdown } from "./dropdown/Dropdown.svelte";
+export { default as DropdownItem } from "./dropdown/DropdownItem.svelte";
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,5 @@
 export { default as Button } from "./components/button/Button.svelte";
+export { default as DropdownButton } from "./components/button/DropdownButton.svelte";
+
+export { default as Dropdown } from "./components/dropdown/Dropdown.svelte";
+export { default as DropdownItem } from "./components/dropdown/DropdownItem.svelte";
